9788e3d2f2a7e7f5bb7249c68c26410990b179fb
Benoit Debordeaux <b.debordeaux2@gmail.com>
2017-01-22 14:48:32 +0100

    angularfire

--- www/js/app.js
+++ www/js/app.js
@@ -3,38 +3,45 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'firebase'])
 
-.controller('ListCtrl', function($scope) {
-	$scope.items = [];
+		.factory('Items', ['$firebaseArray', function ($firebaseArray) {
+			var itemsRef = new Firebase('https://todo-bfa1f.firebaseio.com/items');
+			return $firebaseArray(itemsRef);
+		}])
 
-	$scope.addItem = function () {
-		var name = prompt('What do you need to buy?');
-		if (name) {
-			$scope.items.push({name: name});
-		}
-	};
+		.controller('ListCtrl', function ($scope, $ionicListDelegate, Items) {
+			$scope.items = Items;
 
-	$scope.purchaseItem = function(item) {
-		$scope.item = item;
-		$scope.item['status'] = 'purchased';
-		$ionicListDelegate.closeOptionButtons();
-	};
-	})
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+			$scope.addItem = function () {
+				var name = prompt('What do you need to buy?');
+				if (name) {
+					$scope.items.$add({name: name});
+				}
+			};
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+			$scope.purchaseItem = function (item) {
+				var itemRef = new Firebase('https://todo-bfa1f.firebaseio.com/items/' + item.$id);
+				itemRef.child('status').set('purchased');
+				$ionicListDelegate.closeOptionButtons();
+			};
+		})
+
+		.run(function ($ionicPlatform) {
+			$ionicPlatform.ready(function () {
+				if (window.cordova && window.cordova.plugins.Keyboard) {
+					// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+					// for form inputs)
+					cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+					// Don't remove this line unless you know what you are doing. It stops the viewport
+					// from snapping when text inputs are focused. Ionic handles this internally for
+					// a much nicer keyboard experience.
+					cordova.plugins.Keyboard.disableScroll(true);
+				}
+				if (window.StatusBar) {
+					StatusBar.styleDefault();
+				}
+			});
+		});
+
