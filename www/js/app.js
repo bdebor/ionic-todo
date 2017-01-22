5e4e63659fe9fdcf7619b5c76b694c737f3bd573
Benoit Debordeaux <b.debordeaux2@gmail.com>
2017-01-22 15:06:32 +0100

    cordova plugin add cordova-plugin-dialogs

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'firebase'])
+angular.module('starter', ['ionic', 'firebase', 'ngCordova'])
 
 		.factory('Items', ['$firebaseArray', function ($firebaseArray) {
 			var itemsRef = new Firebase('https://todo-bfa1f.firebaseio.com/items');
